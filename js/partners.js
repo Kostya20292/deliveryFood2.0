8afe049b152732f987428b80777d50314c563beb
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-07-23 14:49:13 +0300

    feat: Сделано получение карточек товаров с сервера

---
+++ js/partners.js
@@ -0,0 +1,10 @@
+const renderItems = (data) => {
+    data.forEach((item) => {
+        console.log(item);
+    });
+};
+
+fetch('https://deliveryfood-74cea-default-rtdb.firebaseio.com/db/partners.json')
+    .then((response) => response.json())
+    .then((data) => renderItems(data))
+    .catch((error) => console.log(error));
